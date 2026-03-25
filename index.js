b4e48806a0c57e5faca1dca0c231814298551887
Igor Passos <haratandeus@gmail.com>
2026-03-25 13:39:29 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+for (let i = 0; i < 10001; i++){ // aqui estou dizendo que enquanto o i for igual a 0, e i for menor que 10001, adicionar 1. no caso vai contar de 1 ate 10000!
+    document.getElementById('teste').innerHTML += i + ', ';
+
+}
